0749a5996c8cfc6edd22a6c2a84708734640f6cb
Santiago Pestarini <santiagonob@gmail.com>
2013-02-14 02:42:36 -0300

    No more using cufón.
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- js/cufonreplace.js
+++
@@ -1,2 +0,0 @@
-Cufon.replace('#headercont h1, #maincont h1, #maincont h2, #maincont h3, #maincont h4, #maincont h5', { fontFamily:'Bebas Neue', hover:'true'});
-Cufon.replace('#menucont ul li a', { fontFamily:'Bebas Neue', hover:'true'});
